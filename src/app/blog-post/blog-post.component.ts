179e71a01e9bfe722aba3d5d3dabf62c3442d466
Metehan Pala <metehanp@gmail.com>
2023-06-25 22:47:22 +0200

    html change

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -7,22 +7,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./blog-post.component.scss']
 })
 export class BlogPostComponent implements OnInit {
-  post: any;
+  public post: any;
+  private postId: number | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      const postId = params['postId'];
+      this.postId = params['postId'];
 
       // Burada, gerçek verileri API'den veya başka bir kaynaktan alabilirsiniz.
       // Şu anda, params nesnesinden postId değerine göre örnek bir veri kullanıyorum.
-      if (postId === '1') {
+      if (this.postId === 1) {
         this.post = {
           title: 'Başlık 1',
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         };
-      } else if (postId === '2') {
+      } else if (this.postId === 2) {
         this.post = {
           title: 'Başlık 2',
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada purus vel lectus porta, eu tempor nulla ultricies.'
